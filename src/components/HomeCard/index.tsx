21ec7b13d8b860fd457f5dfcbc57e359bfd2d8b2
Ron Park <ronpark.dev@gmail.com>
2022-09-02 09:47:45 +0900

    refactor: Extract HomeCard component

---
+++ src/components/HomeCard/index.tsx
@@ -0,0 +1,104 @@
+import classNames from 'classnames/bind'
+import moment from 'moment'
+import React, { useMemo } from 'react'
+
+import styles from './index.module.css'
+
+interface Props {
+  writer: string
+  title: React.ReactNode
+  stack: string
+  updatedDate: Date | string | number
+  estimatedStartDate?: Date | string | number
+  appliedCapacity?: number
+  totalCapacity?: number
+}
+
+const HomeCard: React.FC<Props> = ({
+  writer,
+  title,
+  stack,
+  updatedDate,
+  estimatedStartDate,
+  appliedCapacity,
+  totalCapacity,
+}) => {
+  const cx = useMemo(() => classNames.bind(styles), [])
+
+  const formattedUpdatedDate = useMemo(() => {
+    const diffDays = moment(updatedDate).diff(moment(), 'days')
+
+    if (diffDays > 0) {
+      return `${diffDays}일후`
+    }
+    if (diffDays < 0) {
+      return `${-diffDays}일전`
+    }
+    return '오늘'
+  }, [updatedDate])
+
+  const formattedEstimatedStartDate = useMemo(() => {
+    if (estimatedStartDate === undefined) {
+      return null
+    }
+    return moment(estimatedStartDate).format('YYYY.MM.DD')
+  }, [estimatedStartDate])
+
+  const progress =
+    appliedCapacity !== undefined &&
+    totalCapacity !== undefined &&
+    !Number.isNaN(appliedCapacity) &&
+    !Number.isNaN(totalCapacity)
+      ? (appliedCapacity / totalCapacity) * 100
+      : null
+
+  return (
+    <div className={cx('card p-3 mb-2')}>
+      <div className={cx('d-flex justify-content-between')}>
+        <div className={cx('d-flex flex-row align-items-center')}>
+          <div className={cx('ms-2 c-details')}>
+            <h6 className={cx('mb-0')}>{writer}</h6> <span>{formattedUpdatedDate}</span>
+          </div>
+        </div>
+        <div className={cx('badge')}>
+          {' '}
+          <span>{stack}</span>{' '}
+        </div>
+      </div>
+      <div className={cx('mt-5')}>
+        <h3 className={cx('heading')}>{title}</h3>
+        <div className={cx('mt-5')}>
+          {progress !== null && (
+            <>
+              <div className={cx('progress')}>
+                <div
+                  className={cx('progress-bar')}
+                  role="progressbar"
+                  style={{ width: `${progress}%` }}
+                  aria-valuenow={progress}
+                  aria-valuemin={0}
+                  aria-valuemax={100}></div>
+              </div>
+              <div className={cx('mt-3')}>
+                {' '}
+                <span className={cx('text1')}>
+                  {appliedCapacity} Applied <span className={cx('text2')}>of {totalCapacity} capacity</span>
+                </span>{' '}
+              </div>
+            </>
+          )}
+          {formattedEstimatedStartDate !== null && (
+            <div className="mt-3">
+              {' '}
+              <span className="text1">
+                시작예정일 : <span className="text2">{formattedEstimatedStartDate}</span>
+              </span>{' '}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeCard
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind'
 import React, { useMemo } from 'react'
 
+import HomeCard from '@components/HomeCard'
 import LogoImg from 'assets/home/logo.svg'
 import Slide01Img from 'assets/home/slide01.jpg'
 import Slide02Img from 'assets/home/slide02.jpg'
@@ -51,357 +52,48 @@ const HomePage: React.FC = () => {
       <div className={cx('container mt-5 mb-3')}>
         <div className={cx('row')}>
           <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>조정무님</h6> <span>1일전</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>React</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
+            <HomeCard
+              writer={'조정무님'}
+              updatedDate={new Date(new Date().getTime() - 1 * 24 * 60 * 60 * 1000)}
+              title={
+                <>
                   [사이드프로젝트] <br />
                   동물 NFT 서비 스 - 프론트, 백엔드 개발자 모집
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '30%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      시작예정일 : <span className={cx('text2')}>2022.08.29</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
+                </>
+              }
+              stack={'React'}
+              estimatedStartDate={new Date('2022-08-29')}
+            />
           </div>
           <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>Dribbble</h6> <span>4 days ago</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>Product</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
-                  Junior Product
-                  <br />
+            <HomeCard
+              writer={'Dribbble'}
+              updatedDate={new Date(new Date().getTime() - 4 * 24 * 60 * 60 * 1000)}
+              title={
+                <>
+                  Junior Product <br />
                   Designer-Singapore
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '50%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      42 Applied <span className={cx('text2')}>of 70 capacity</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
+                </>
+              }
+              stack={'Product'}
+              appliedCapacity={42}
+              totalCapacity={70}
+            />
           </div>
           <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>Reddit</h6> <span>2 days ago</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>Design</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
+            <HomeCard
+              writer={'Reddit'}
+              updatedDate={new Date(new Date().getTime() - 2 * 24 * 60 * 60 * 1000)}
+              title={
+                <>
                   Software Architect <br />
                   Java - USA
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '70%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      52 Applied <span className={cx('text2')}>of 100 capacity</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={cx('container mt-5 mb-3')}>
-        <div className={cx('row')}>
-          <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>조정무님</h6> <span>1일전</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>React</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
-                  [사이드프로젝트] <br />
-                  동물 NFT 서비스 - 프론트, 백엔드 개발자 모집
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '30%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      시작예정일 : <span className={cx('text2')}>2022.08.29</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>Dribbble</h6> <span>4 days ago</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>Product</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
-                  Junior Product
-                  <br />
-                  Designer-Singapore
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '50%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      42 Applied <span className={cx('text2')}>of 70 capacity</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>Reddit</h6> <span>2 days ago</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>Design</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
-                  Software Architect <br />
-                  Java - USA
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '70%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      52 Applied <span className={cx('text2')}>of 100 capacity</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={cx('container mt-5 mb-3')}>
-        <div className={cx('row')}>
-          <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>조정무님</h6> <span>1일전</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>React</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
-                  [사이드프로젝트] <br />
-                  동물 NFT 서비 스 - 프론트, 백엔드 개발자 모집
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '30%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      시작예정일 : <span className={cx('text2')}>2022.08.29</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>Dribbble</h6> <span>4 days ago</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>Product</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
-                  Junior Product
-                  <br />
-                  Designer-Singapore
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '50%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      42 Applied <span className={cx('text2')}>of 70 capacity</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={cx('col-md-4')}>
-            <div className={cx('card p-3 mb-2')}>
-              <div className={cx('d-flex justify-content-between')}>
-                <div className={cx('d-flex flex-row align-items-center')}>
-                  <div className={cx('ms-2 c-details')}>
-                    <h6 className={cx('mb-0')}>Reddit</h6> <span>2 days ago</span>
-                  </div>
-                </div>
-                <div className={cx('badge')}>
-                  {' '}
-                  <span>Design</span>{' '}
-                </div>
-              </div>
-              <div className={cx('mt-5')}>
-                <h3 className={cx('heading')}>
-                  Software Architect <br />
-                  Java - USA
-                </h3>
-                <div className={cx('mt-5')}>
-                  <div className={cx('progress')}>
-                    <div
-                      className={cx('progress-bar')}
-                      role="progressbar"
-                      style={{ width: '70%' }}
-                      aria-valuenow={50}
-                      aria-valuemin={0}
-                      aria-valuemax={100}></div>
-                  </div>
-                  <div className={cx('mt-3')}>
-                    {' '}
-                    <span className={cx('text1')}>
-                      52 Applied <span className={cx('text2')}>of 100 capacity</span>
-                    </span>{' '}
-                  </div>
-                </div>
-              </div>
-            </div>
+                </>
+              }
+              stack={'Design'}
+              appliedCapacity={52}
+              totalCapacity={100}
+            />
           </div>
         </div>
       </div>
