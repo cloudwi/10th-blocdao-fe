4c70045796a8764d2bea5c6ca2509ed36fb17a3d
Ron Park <ronpark.dev@gmail.com>
2022-09-12 19:33:06 +0900

    fix: Modify base url of members api

--- src/services/member.ts
+++ src/services/member.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://blocdao.herokuapp.com/api/member'
+const BASE_URL = 'https://blocdao.herokuapp.com/api/members'
 
 const memberAxios = axios.create({ baseURL: BASE_URL })
 
@@ -9,7 +9,7 @@ interface LoginParams {
 }
 
 const signIn = async ({ token }: LoginParams) => {
-  await memberAxios.get('/login', { headers: { Authorization: `Bearer ${token}` } })
+  await memberAxios.get('/', { headers: { Authorization: `Bearer ${token}` } })
 }
 
 interface SignUpParam {
@@ -24,7 +24,7 @@ interface SignUpParam {
 
 const signUp = async ({ token, nickName, imageUrl, email, phone, profileLink, memberStacks }: SignUpParam) => {
   await memberAxios.post(
-    '/signup',
+    '/',
     { nickName, imageUrl, email, phone, profileLink, memberStacks },
     { headers: { Authorization: `Bearer ${token}` } },
   )
